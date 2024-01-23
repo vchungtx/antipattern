ec957190909102736d10696162db0734e3e0bce1
vchungtx <vchungtx@gmail.com>
2024-01-23 22:41:30 +0700

    init

---
+++ chattyio/src/main/java/com/example/chattyio/ChattyioApplication.java
@@ -0,0 +1,13 @@
+package com.example.chattyio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattyioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattyioApplication.class, args);
+	}
+
+}
---
+++ chattyio/src/test/java/com/example/chattyio/ChattyioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattyio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
